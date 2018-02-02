d537ef9b89db1cfc7a465fd0c85121e6b9750b52
tanguyeqt <tanguyequinet@outlook.fr>
2018-02-02 14:11:52 +0100

    Ajout du TP QuizzApp

---
+++ UE2 - Développement d’applications native Android pour mobile/Android/QuizzApp/app/src/main/java/aston/fr/quizzapp/quizz/QuizzActivity.java
@@ -0,0 +1,15 @@
+package aston.fr.quizzapp.quizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import aston.fr.quizzapp.R;
+
+public class QuizzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizz);
+    }
+}
