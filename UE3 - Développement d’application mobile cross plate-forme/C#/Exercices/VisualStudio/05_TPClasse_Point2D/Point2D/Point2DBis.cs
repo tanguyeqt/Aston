535864e3cb527c7cb2086ecf623c90e85e05b71d
tanguyeqttanguyeqt <tanguyequinet@outlook.fr>
2018-02-27 16:37:56 +0100

    correction prof tp point2d

---
+++ UE3 - Développement d’application mobile cross plate-forme/C#/Exercices/VisualStudio/05_TPClasse_Point2D/Point2D/Point2DBis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point2D
+{
+    class Point2DBis
+    {
+        public static int Compteur { get; private set; }
+
+        public Point2DBis()
+        {
+            Compteur++;
+        }
+
+        private int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+   
+        public int Y { get; set; }
+
+        public void Afficher()
+        {
+            Console.WriteLine(string.Format("Votre point à pour abcisse {0} et pour ordonnée {1}", this.X, this.Y));
+        }
+
+        public void Translater(int dX, int dY)
+        {
+            this.x += dX;
+            this.Y += dY;
+        }
+    }
+}
