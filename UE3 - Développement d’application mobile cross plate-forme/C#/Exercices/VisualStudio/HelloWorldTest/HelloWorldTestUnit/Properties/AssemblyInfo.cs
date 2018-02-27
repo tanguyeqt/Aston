d39a2667b39ca3607bb17d817b7a5cc321bbb5c1
tanguyeqttanguyeqt <tanguyequinet@outlook.fr>
2018-02-27 13:40:04 +0100

    Ajout cours et tp

---
+++ UE3 - Développement d’application mobile cross plate-forme/C#/Exercices/VisualStudio/HelloWorldTest/HelloWorldTestUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTestUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTestUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4acd8284-d96b-42ed-a601-dcb4f7b110b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UE3 - Développement d’application mobile cross plate-forme/C#/Exercices/VisualStudio/HelloWorldTest/HelloWorldTestUnit/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace HelloWorldTestUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+           
+
+
+
+        }
+    }
+}
